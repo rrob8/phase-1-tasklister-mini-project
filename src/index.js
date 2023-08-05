e9fce2021b1355525623e0fc65263ad457517636
Robert Bubnik <rbubnik@gmail.com>
2023-08-05 13:30:19 -0400

    passed test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let field = document.getElementById("create-task-form");
+  field.addEventListener('submit', (e) => {  // listen for a submit event on the form element
+    e.preventDefault() ; // prevent default action of reloading the page, target submit
+   let newEntry = e.target['new-task-description'].value;
+    addEntry(newEntry);
+    field.reset();
+  });
 });
+
+  
+ 
+  
+  
+
+  
+
+
+function addEntry (entry) {
+let p = document.createElement('p');
+let btn = document.createElement('button');
+btn.addEventListener('click', handleDelete)
+btn.textContent = 'x'
+p.textContent = `${entry} `
+p.appendChild(btn)
+document.getElementById('tasks').appendChild(p)
+let drop = document.createElement('select')
+p.appendChild(drop)
+
+}
+
+function handleDelete (e) {
+e.target.parentNode.remove()
+}
